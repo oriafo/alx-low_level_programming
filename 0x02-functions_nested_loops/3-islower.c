899d676ce1420b98e3e34e3c6d9538abd0f248a8
oriafo <abrahamibharunujele@gmail.com>
2022-03-15 21:37:46 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @c: c is a parameter is lower or uppercase
+ * Return: 0 or 1
+*/
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
