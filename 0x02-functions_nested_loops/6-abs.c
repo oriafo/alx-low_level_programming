448293d8c5de246f5a50a87a44af0976cfc686c3
oriafo <abrahamibharunujele@gmail.com>
2022-03-15 21:44:09 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value
+ * @num : number to check
+ * Return: return the absolute value
+*/
+
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (num * -1);
+}
