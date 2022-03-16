5301a27702199eda2e33680bc8dbce5cf38348a2
oriafo <abrahamibharunujele@gmail.com>
2022-03-15 22:01:29 -0700

    _putchar

---
+++ 8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day of Jack Bauer
+ * Return: void
+*/
+
+void jack_bauer(void)
+{
+	int m1;
+	int m2;
+	int h1;
+	int h2;
+	int a = 9;
+
+	h2 = 0;
+	while (h2 <= 2)
+	{
+		if (h2 == 2)
+		{
+			/* restrain to 23h instead of 29h */
+			a = 3;
+		}
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
+	}
+}
