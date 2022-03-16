4dab126a5ca1667a19bfcc1543c51090441b74fb
oriafo <abrahamibharunujele@gmail.com>
2022-03-16 09:05:13 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,30 +5,27 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	int i = 0;
-	unsigned long int sum1, sum2, sum;
-
-	sum1 = 0;
-	sum2 = 0;
+	sum3 = 0;
+	sum5 = 0;
 	sum = 0;
-	while (i <= 1024)
-		{	
-			if ((i % 3) == 0)
-			{
-				sum1 = sum1 + i;
-				i++;
-			}
-			else if ((i % 5) == 0)
-			{
-				sum2 = sum2 + i;
-				i++;
-			}
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
 		}
-	sum = sum1 + sum2;
+	}
+	sum = sum3 + sum5;
 	printf("%lu\n", sum);
 	return (0);
 }
+
